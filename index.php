f3494eeab8dec534e9bec5e23e0610251176409c
Dervis <dervisgomez77@gmail.com>
2017-06-05 23:01:29 -0400

    Graba y genera base64

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Grabar Audio</title>
+	<link href="css/estilo.css" rel="stylesheet" />
+	<script src="js/recorder.js"></script>
+		<script src="js/Fr.voice.js"></script>
+    <script src="js/jquery-3.2.1.min.js"></script>
+		<script src="js/app.js"></script>
+	
+</head>
+<body>
+	<a class="button one" id="iniciar">iniciar</a>
+	<a class="button one" id="finalizar">finalizar</a>
+      
+</body>
+</html>
